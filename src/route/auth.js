15c9e4d9d5c8d89915fec00213e50b8f6d08eb03
bentfor1 <vasyl.pidoyma@gmail.com>
2023-08-26 07:43:39 +0300

    my homework

---
+++ src/route/auth.js
@@ -0,0 +1,35 @@
+// Підключаємо роутер до бек-енду
+const express = require('express')
+const router = express.Router()
+
+const { User } = require('../class/user')
+
+router.get('/signup', function (req, res) {
+  return res.render('signup', {
+    name: 'signup',
+    component: [
+      'back-button',
+      'field',
+      'field-password',
+      'field-checkbox',
+      'field-select',
+    ],
+    title: 'Signup page',
+    data: {
+      role: [
+        { value: User.USER_ROLE.USER, text: 'Користувач' },
+        {
+          value: User.USER_ROLE.ADMIN,
+          text: 'Адміністратор',
+        },
+        {
+          value: User.USER_ROLE.DEVELOPER,
+          text: 'Розробник',
+        },
+      ],
+    },
+  })
+})
+
+// Експортуємо глобальний роутер
+module.exports = router
--- src/route/index.js
+++ src/route/index.js
@@ -2,12 +2,31 @@
 const express = require('express')
 const router = express.Router()
 
+// тут вводимо шлях (PATH) до сторінки
+router.get('/', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // сюди вводимо назву файлу з контейнеру
+  res.render('index', {
+    // вказуємо назву контейнера
+    name: 'index',
+    // вказуємо назву компонентів
+    component: [],
+
+    // вказуємо назву сторінки
+    title: 'Home pege',
+    // вказуємо дані
+    data: {},
+  })
+  // сюди вводимо JSON дані
+})
+
 // Підключіть файли роутів
-const test = require('./test')
+const auth = require('./auth')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+router.use('/', auth)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
